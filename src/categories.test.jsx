9ae4413b344b808d22362c4712b291273036531d
hyjoong <maplesosser@naver.com>
2022-01-08 12:55:56 +0900

    test: categories render test

---
+++ src/categories.test.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import Categories from './categories';
+
+describe('Categories', () => {
+  it('render', () => {
+    render(<Categories />);
+  });
+});
