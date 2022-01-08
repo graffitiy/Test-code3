24fa7a40a740e7ac1efff5fad67cef688d314795
hyjoong <maplesosser@naver.com>
2022-01-08 13:31:02 +0900

    test: 카테고리 클릭시 V 표시되는 test

--- src/categories.test.jsx
+++ src/categories.test.jsx
@@ -1,10 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Categories from './categories';
 import { CATEGORIES } from './constants';
 
 describe('Categories', () => {
+  const renderComponent = () => render(<Categories categories={CATEGORIES} />);
   it('render', () => {
-    const { container } = render(<Categories categories={CATEGORIES} />);
+    const { container } = renderComponent();
     expect(container).toHaveTextContent('한식');
+  });
+
+  it('카테고리를 클릭하면 V가 표시된다.', () => {
+    const { container, getByRole } = renderComponent();
+    const button = getByRole('button', { name: '한식' });
+    fireEvent.click(button);
+    expect(container).toHaveTextContent('한식(v)');
   });
 });
