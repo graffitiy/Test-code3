b7b5b615341c7caf8a75d89d5b0e915998c5c3b5
hyjoong <maplesosser@naver.com>
2022-01-08 13:01:00 +0900

    test: categoriesContainer render test

---
+++ src/categoriesContainer.test.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import CategoriesContainer from './categoriesContainer';
+
+describe('CategoriesContainer', () => {
+  it('render', () => {
+    render(<CategoriesContainer />);
+  });
+});
