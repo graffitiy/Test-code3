32c9db97ba1d33b07b8b4468e19d1bb836664f1d
hyjoong <maplesosser@naver.com>
2022-01-09 12:57:51 +0900

    feat: 카테고리 유무 context 추가

--- src/categories.test.jsx
+++ src/categories.test.jsx
@@ -11,16 +11,20 @@ describe('Categories', () => {
     expect(container).toHaveTextContent('한식');
   });
 
-  it('카테고리를 클릭하면 V가 표시된다.', () => {
-    const { container, getByRole } = renderComponent(CATEGORIES);
-    const button = getByRole('button', { name: '한식' });
-    fireEvent.click(button);
-    expect(container).toHaveTextContent('한식(V)');
+  context('카테고리가 있을 때', () => {
+    it('카테고리를 클릭하면 V가 표시된다.', () => {
+      const { container, getByRole } = renderComponent(CATEGORIES);
+      const button = getByRole('button', { name: '한식' });
+      fireEvent.click(button);
+      expect(container).toHaveTextContent('한식(V)');
+    });
   });
 
-  it('카테고리가 없으면 메세지가 출력된다.', () => {
-    const { container } = renderComponent([]);
+  context('카테고리가 없을 때', () => {
+    it('카테고리가 없다는 메세지를 확인할 수 있다', () => {
+      const { container } = renderComponent([]);
 
-    expect(container).toHaveTextContent('카테고리가 존재하지 않습니다.');
+      expect(container).toHaveTextContent('카테고리가 존재하지 않습니다.');
+    });
   });
 });
