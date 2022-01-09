6634754b00d51b195570b57ec003d186137964da
hyjoong <maplesosser@naver.com>
2022-01-09 17:55:04 +0900

    test: loadRegions action test

--- src/action.test.js
+++ src/action.test.js
@@ -1,13 +1,14 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import fetchCategories from './api/api';
-import { CATEGORIES } from './constants';
-import { loadCategories } from './actions';
+import { fetchRegions, fetchCategories } from './api/api';
+import { CATEGORIES, REGIONS } from './constants';
+import { loadCategories, loadRegions } from './actions';
 
 jest.mock('./api/api');
 
 const mockStore = configureStore([thunk]);
 fetchCategories.mockResolvedValue(CATEGORIES);
+fetchRegions.mockResolvedValue(REGIONS);
 
 describe('actions', () => {
   describe('loadCategories', () => {
@@ -17,6 +18,16 @@ describe('actions', () => {
       const actions = store.getActions();
 
       expect(actions[0].payload.categories).toEqual(CATEGORIES);
+    });
+  });
+
+  describe('loadRegions', () => {
+    it('setRegions를 호출한다', async () => {
+      const store = mockStore({});
+      await store.dispatch(loadRegions());
+      const actions = store.getActions();
+
+      expect(actions[0].payload.regions).toEqual(REGIONS);
     });
   });
 });
