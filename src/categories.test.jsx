5490b4f6c057eef7b691b9f034abba0efbbb91e4
hyjoong <maplesosser@naver.com>
2022-01-09 12:35:42 +0900

    test: 카테고리 없을 경우 test

--- src/categories.test.jsx
+++ src/categories.test.jsx
@@ -3,16 +3,24 @@ import Categories from './categories';
 import { CATEGORIES } from './constants';
 
 describe('Categories', () => {
-  const renderComponent = () => render(<Categories categories={CATEGORIES} />);
+  const renderComponent = (categories) => render(<Categories categories={categories} />);
+
   it('render', () => {
-    const { container } = renderComponent();
+    const { container } = renderComponent(CATEGORIES);
+
     expect(container).toHaveTextContent('한식');
   });
 
   it('카테고리를 클릭하면 V가 표시된다.', () => {
-    const { container, getByRole } = renderComponent();
+    const { container, getByRole } = renderComponent(CATEGORIES);
     const button = getByRole('button', { name: '한식' });
     fireEvent.click(button);
     expect(container).toHaveTextContent('한식(V)');
+  });
+
+  it('카테고리가 없으면 메세지가 출력된다.', () => {
+    const { container } = renderComponent([]);
+
+    expect(container).toHaveTextContent('카테고리가 존재하지 않습니다.');
   });
 });
