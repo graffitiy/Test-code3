1097ce956fb80d56de59638e5a22b6d41c2b81e6
hyjoong <maplesosser@naver.com>
2022-01-10 22:11:36 +0900

    test: restaurants 목록 조회 test

--- src/action.test.js
+++ src/action.test.js
@@ -1,14 +1,16 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { fetchRegions, fetchCategories } from './api/api';
-import { CATEGORIES, REGIONS } from './constants';
-import { loadCategories, loadRegions } from './actions';
+import { fetchRegions, fetchCategories, fetchRestaurants } from './api/api';
+import { CATEGORIES, REGIONS, RESTAURANTS } from './constants';
+import { loadCategories, loadRegions, loadRestaurants } from './actions';
 
 jest.mock('./api/api');
 
 const mockStore = configureStore([thunk]);
+
 fetchCategories.mockResolvedValue(CATEGORIES);
 fetchRegions.mockResolvedValue(REGIONS);
+fetchRestaurants.mockResolvedValue(RESTAURANTS);
 
 describe('actions', () => {
   describe('loadCategories', () => {
@@ -28,6 +30,17 @@ describe('actions', () => {
       const actions = store.getActions();
 
       expect(actions[0].payload.regions).toEqual(REGIONS);
+    });
+  });
+
+  describe('loadRestaurants', () => {
+    it('setRestaurants를 호출한다', async () => {
+      const store = mockStore({});
+      await store.dispatch(loadRestaurants());
+
+      const actions = store.getActions();
+
+      expect(actions[0].payload.restaurants).toEqual(RESTAURANTS);
     });
   });
 });
