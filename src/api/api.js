8f39a669371c8985dea05ff06e6a8a0484ba6bc3
hyjoong <maplesosser@naver.com>
2022-01-11 13:11:34 +0900

    test: api test

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,25 @@
 import { GET_CATEGORIES, GET_REGIONS, GET_RESTAURANTS } from './urls';
 
+export const fetchModule = async (url) => {
+  try {
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 export const fetchCategories = async () => {
-  const response = await fetch(GET_CATEGORIES);
+  const response = await fetchModule(GET_CATEGORIES);
   return response.json();
 };
 
 export const fetchRegions = async () => {
-  const regions = await fetch(GET_REGIONS);
+  const regions = await fetchModule(GET_REGIONS);
   return regions.json();
 };
 
-export const fetchRestaurants = async (region, categoryId) => {
-  const restaurants = await fetch(GET_RESTAURANTS(region, categoryId));
+export const fetchRestaurants = async (region = '', categoryId = 0) => {
+  const restaurants = await fetchModule(GET_RESTAURANTS(region, categoryId));
   return restaurants.json();
 };
